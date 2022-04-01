d0dabe67bdf7aecbcf9252c316a0c6aebf6e1377
Janis Neibergs <neibis21@gmail.com>
2022-04-01 23:40:46 +0300

    test first commit

--- src/main/java/io/codelex/flowcontrol/practice/TenBillion.java
+++ src/main/java/io/codelex/flowcontrol/practice/TenBillion.java
@@ -10,45 +10,34 @@ public class TenBillion {
         Scanner in = new Scanner(System.in);
         System.out.print("Input an integer number less than ten billion: ");
 
-        String num = in.nextLine();
-        String n1 = num.replaceAll("\\D", "");
+        Long n = in.nextLong();
         int digits = 0;
-        if (num.length() == n1.length()) {
-            long n = Long.parseLong(num);
-            if (n < 0) {
-                n *= -1;
-            }
-
-            if (n > 10000000000L) {
-                System.out.println("Number is greater than 10,000,000,000!");
-            } else {
-
-                if (n <= 99) {
-                    digits = 2;
-                } else if (n <= 999) {
-                    digits = 3;
-                } else if (n <= 9999) {
-                    digits = 4;
-                } else if (n <= 99999) {
-                    digits = 5;
-                } else if (n <= 999999) {
-                    digits = 6;
-                } else if (n <= 9999999) {
-                    digits = 7;
-                } else if (n <= 99999999) {
-                    digits = 8;
-                } else if (n <= 999999999) {
-                    digits = 9;
-                } else if (n == 10000000000L) {
-                    digits = 10;
-                }
-
-                System.out.println("Number of digits in the number: " + digits);
-            }
+        if (n > 10000000000L) {
+            System.out.println("Number is greater than 10,000,000,000!");
         } else {
-            System.out.println("This is not a digit");
+            if (n <= 99) {
+                digits = 2;
+            } else if (n <= 999) {
+                digits = 3;
+            } else if (n <= 9999) {
+                digits = 4;
+            } else if (n <= 99999) {
+                digits = 5;
+            } else if (n <= 999999) {
+                digits = 6;
+            } else if (n <= 9999999) {
+                digits = 7;
+            } else if (n <= 99999999) {
+                digits = 8;
+            } else if (n <= 999999999) {
+                digits = 9;
+            } else if (n == 10000000000L) {
+                digits = 10;
+            }
+            System.out.println("Number of digits in the number: " + digits);
         }
-
     }
 
 }
+
+
