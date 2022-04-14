965f95bc18ac16259d920d765ce3174c30543579
Janis Neibergs <neibis21@gmail.com>
2022-04-14 10:18:10 +0300

    Enum second commit

--- src/main/java/io/codelex/enums/ex2/Ex2.java
+++ src/main/java/io/codelex/enums/ex2/Ex2.java
@@ -1,6 +1,5 @@
 package io.codelex.enums.ex2;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Ex2 {
@@ -18,20 +17,29 @@ public class Ex2 {
             String player = in.next();
             String playerMove = player.toLowerCase();
             String computerMove = getComputerMove();
-            if (playerMove.equals("q")) quit = false;
-            else if (playerMove.equals(computerMove))
+            if (playerMove.equals("q")) {
+                quit = false;
+            } else if (playerMove.equals(computerMove)) {
                 System.out.println("Tie!");
-
-            else if (playerMove.equals(sps.STONE.key)) {
-                if (computerMove.equals(sps.PAPER.key)) System.out.println("Paper wraps stone, I won");
-                else System.out.println("You won!");
+            } else if (playerMove.equals(sps.STONE.key)) {
+                if (computerMove.equals(sps.PAPER.key)) {
+                    System.out.println("Paper wraps stone, I won");
+                } else {
+                    System.out.println("You won!");
+                }
 
             } else if (playerMove.equals(sps.PAPER.key)) {
-                if (computerMove.equals(sps.SCISSORS.key)) System.out.println("Scissors cuts paper, I won!");
-                else System.out.println("You won!");
+                if (computerMove.equals(sps.SCISSORS.key)) {
+                    System.out.println("Scissors cuts paper, I won!");
+                } else {
+                    System.out.println("You won!");
+                }
             } else {
-                if (computerMove.equals(sps.STONE.key)) System.out.println("Stone breaks scissors");
-                else System.out.println("You won!");
+                if (computerMove.equals(sps.STONE.key)) {
+                    System.out.println("Stone breaks scissors");
+                } else {
+                    System.out.println("You won!");
+                }
             }
         }
 
@@ -39,25 +47,11 @@ public class Ex2 {
     }
 
     public static String getComputerMove() {
-        Sps sps = Sps.SCISSORS;
-        String computermove;
-        Random random = new Random();
 
+        Sps sps = Sps.PAPER.getRandomValue();
 
-        int input = random.nextInt(3) + 1;
-        if (input == 1) {
-            computermove = sps.STONE.key;
-            System.out.println("My turn: " + sps.STONE);
-        } else if (input == 2) {
-            computermove = sps.PAPER.key;
-            System.out.println("My turn: " + sps.PAPER);
-        } else {
-            computermove = sps.SCISSORS.key;
-            System.out.println("My turn: " + sps.SCISSORS);
-        }
-
-        System.out.println();
-        return computermove;
+        System.out.println("My turn: " + sps);
+        return sps.key;
     }
 
 
--- src/main/java/io/codelex/enums/ex2/Sps.java
+++ src/main/java/io/codelex/enums/ex2/Sps.java
@@ -1,13 +1,24 @@
 package io.codelex.enums.ex2;
 
+import java.util.Random;
+
 public enum Sps {
+
     SCISSORS("s"),
     PAPER("p"),
     STONE("t");
 
-    public final String key;
+    String key;
 
-    private Sps(String key) {
+    Sps(String key) {
+
         this.key = key;
+
+    }
+
+    public Sps getRandomValue() {
+
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
     }
 }
